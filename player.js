3dc1cac1a6d7ef04650e7e0487d78678d99ebf37
vineela3 <76429823+vineela3@users.noreply.github.com>
2021-07-07 09:33:42 +0530

    Update player.js

--- player.js
+++ player.js
@@ -6,7 +6,7 @@ class Player{
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
-        this.image=loadImage("assets/player.png");
+        this.image=loadImage("player.png");
 
         World.add(world,this.body);
 
