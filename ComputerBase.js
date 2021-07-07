dabda39efcd6f0405246b49dff4493fb939d8ed3
vineela3 <76429823+vineela3@users.noreply.github.com>
2021-07-07 09:33:06 +0530

    Update ComputerBase.js

--- ComputerBase.js
+++ ComputerBase.js
@@ -6,7 +6,7 @@ class ComputerBase{
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
-        this.image=loadImage("assets/base2.png");
+        this.image=loadImage("base2.png");
 
         World.add(world,this.body);
 
